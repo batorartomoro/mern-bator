87d605a8723b307fbf9d51d02886260fd0fefc66
BatorArtoMoro <batorartomoro@gmail.com>
2024-02-27 18:25:11 +0700

    untuk dirender

--- api/index.js
+++ api/index.js
@@ -8,6 +8,7 @@ import timRoutes from "./routes/tim.route.js";
 import projectRoutes from "./routes/project.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
+import path from 'path';
 
 dotenv.config();
 mongoose.connect(
@@ -16,7 +17,9 @@ mongoose.connect(
     ()=>{console.log('Berhasil koneksi ke Monggo DB..!!')},
 ).catch(err=>{
     console.log(err);
-})
+});
+const __dirname = path.resolve();
+
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -30,6 +33,12 @@ app.use('/api/post',postRoutes);
 app.use('/api/project',projectRoutes);
 app.use('/api/tim',timRoutes);
 app.use('/api/comment',commentRoutes);
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'));
+})
 
 app.use((err,req,res,next)=>{
     const statusCode=err.statusCode || 500;
